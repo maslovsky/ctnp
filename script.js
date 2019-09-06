be88f4ed5637c40d5d846bd0ff8f7475caa7ec3a
asa <s>
2019-09-06 19:14:34 +0300

    remove js parallax, use css parallax instead

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
   const glide = new Glide('.glide').mount();
-
-  const containersToParallax = document.querySelectorAll('.section.pros, .section.price');
-
-  window.addEventListener('scroll', () => {
-    const offset = window.pageYOffset;
-
-    containersToParallax.forEach(x => x.style['background-position-y'] = `-${-(offset - x.offsetTop) / 5}px`);
-  });
 });
